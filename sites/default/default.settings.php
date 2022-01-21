0be43d818654007c846eca30e53aefa1a97f4784
peeyushjvvvv-it <97091489+peeyushjvvvv-it@users.noreply.github.com>
2022-01-21 10:32:40 +0530

    debug

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 $config['system.logging']['error_level'] = 'verbose';
+echo 'debug_peeyush';
 // phpcs:ignoreFile
 
 /**
